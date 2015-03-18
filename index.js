3695d3049bda84c90b1db0ea5673fffb5090debe
Richard Moore <Rmoore424@gmail.com>
2015-03-18 16:28:49 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+#! /usr/bin/env node
+
+var fs = require("fs");
+var path = require("path");
+var Git = require("nodegit");
+
+var pathToRepo = path.resolve(__dirname);
+
+Git.Repository.open(pathToRepo).then(function (repo) {
+	console.log(repo);
+});
+
+var getMostRecentCommit = function(repository) {
+	return repository.getBranchCommit("master");
+};
+
+var getCommitMessage = function(commit) {
+	return commit.message();
+}
+
+var walk = function (dir, done) {
+	var results = [];
+	fs.readdir(dir, function (err, files) {
+		if (err) return done(err);
+		var pending = files.length;
+		if (!pending) return done(null, results);
+		files.forEach(function (file) {
+			file = path.resolve(dir, file);
+			fs.stat(file, function (err, stat) {
+				if (stat && stat.isDirectory()) {
+					walk(file, function (err, res) {
+						results = results.concat(res);
+						if (!--pending) done(null, results);
+					});
+				} else {
+					results.push(file);
+					if (!--pending) done(null, results);
+				}
+			});
+		});
+	});
+}
+
+// walk(__dirname, function (err, results) {
+// 	results.forEach(function (file) {
+// 		fs.watchFile(file, function (err, data) {
+// 			fs.readFile(file, {encoding: 'utf-8'}, function (err, watchedFile) {
+// 				console.log(watchedFile);
+// 			});
+// 		});
+// 	});
+// });
+//addcomment
+//module.exports = currentFile;
+		// files.forEach(function (file) {
+		// 	console.log(file);
+		// 	fs.watchFile(file, function (err, data) {
+		// 		fs.readFile(file, {encoding: 'utf-8'}, function (err, data) {
+		// 			// currentFile = file;
+		// 			console.log(data);
+		// 			//addcomment
+		// 		});
+		// 	});
+		// });
