917d4232db165e3cee384c6ff7a75fe4f30cbf82
Richard Moore <Rmoore424@gmail.com>
2015-03-19 12:27:37 -0400

    File Updated

--- index.js
+++ index.js
@@ -13,51 +13,53 @@ dir.files(__dirname, function (err, files) {
 			fs.watch(file, function (event, filename) {
 				repo.add("--all", function (err) {
 					repo.commit("File Updated", function (err) {
-						return null;
+						repo.remote_list(function (remotes) {
+							console.log(remotes);
+						});
 					});
 				});
 			});
 		}
 	});
 });
+//add comment
+// var walk = function (dir, done) {
+// 	var results = [];
+// 	fs.readdir(dir, function (err, files) {
+// 		if (err) return done(err);
+// 		var pending = files.length;
+// 		if (!pending) return done(null, results);
+// 		files.forEach(function (file) {
+// 			file = path.resolve(dir, file);
+// 			fs.stat(file, function (err, stat) {
+// 				if (stat && stat.isDirectory()) {
+// 					walk(file, function (err, res) {
+// 						results = results.concat(res);
+// 						if (!--pending) done(null, results);
+// 					});
+// 				} else {
+// 					results.push(file);
+// 					if (!--pending) done(null, results);
+// 				}
+// 			});
+// 		});
+// 	});
+// }
 
-var walk = function (dir, done) {
-	var results = [];
-	fs.readdir(dir, function (err, files) {
-		if (err) return done(err);
-		var pending = files.length;
-		if (!pending) return done(null, results);
-		files.forEach(function (file) {
-			file = path.resolve(dir, file);
-			fs.stat(file, function (err, stat) {
-				if (stat && stat.isDirectory()) {
-					walk(file, function (err, res) {
-						results = results.concat(res);
-						if (!--pending) done(null, results);
-					});
-				} else {
-					results.push(file);
-					if (!--pending) done(null, results);
-				}
-			});
-		});
-	});
-}
-
-var watchFilesAndCommit = function (err, results) {
-	results.forEach(function (file) {
-		fs.watch(file, function (event, filename) {
-			console.log(event);
-			repo.add('--all', function (err) {
-				repo.commit('File Updated', function (err) {
-					repo.commits(function (err, commits) {
-						return commits;
-					});
-				});
-			});
-		});
-	});
-};
+// var watchFilesAndCommit = function (err, results) {
+// 	results.forEach(function (file) {
+// 		fs.watch(file, function (event, filename) {
+// 			console.log(event);
+// 			repo.add('--all', function (err) {
+// 				repo.commit('File Updated', function (err) {
+// 					repo.commits(function (err, commits) {
+// 						return commits;
+// 					});
+// 				});
+// 			});
+// 		});
+// 	});
+// };
 //add comment
 // walk(__dirname, watchFilesAndCommit);
 //});
