8648d0437fbdfb05cfbc6acb08981d83493c6d13
Richard Moore <Rmoore424@gmail.com>
2015-03-19 14:35:56 -0400

    File Updated

--- index.js
+++ index.js
@@ -6,59 +6,23 @@ var git = require('gift');
 var dir = require("node-dir");
 var repo = git(__dirname);
 
-dir.files(__dirname, function (err, files) {
-	if (err) throw err;
-	files.forEach(function (file) {
-		if (!file.match(/node_modules/) && !file.match(/.git/)) {
-			fs.watchFile(file, function (curr, prev) {
-				 repo.add(file, function (err) {
-				 	 repo.commit("File Updated", function (err) {
-						repo.remote_push("origin", "master", function (err) {
-							if (err) console.log(err);
-						});
-				 	 });
-				 });
+module.exports = {
+	filewatcher: function () {
+		dir.files(__dirname, function (err, files) {
+			if (err) throw err;
+			files.forEach(function (file) {
+				if (!file.match(/node_modules/) && !file.match(/.git/)) {
+					fs.watchFile(file, function (curr, prev) {
+						 repo.add(file, function (err) {
+						 	 repo.commit("File Updated", function (err) {
+								repo.remote_push("origin", "master", function (err) {
+									if (err) console.log(err);
+								});
+						 	 });
+						 });
+					});
+				}
 			});
-		}
-	});
-});
-// var walk = function (dir, done) {
-// 	var results = [];
-// 	fs.readdir(dir, function (err, files) {
-// 		if (err) return done(err);
-// 		var pending = files.length;
-// 		if (!pending) return done(null, results);
-// 		files.forEach(function (file) {
-// 			file = path.resolve(dir, file);
-// 			fs.stat(file, function (err, stat) {
-// 				if (stat && stat.isDirectory()) {
-// 					walk(file, function (err, res) {
-// 						results = results.concat(res);
-// 						if (!--pending) done(null, results);
-// 					});
-// 				} else {
-// 					results.push(file);
-// 					if (!--pending) done(null, results);
-// 				}
-// 			});
-// 		});
-// 	});
-// }
-
-// var watchFilesAndCommit = function (err, results) {
-// 	results.forEach(function (file) {
-// 		fs.watch(file, function (event, filename) {
-// 			console.log(event);
-// 			repo.add('--all', function (err) {
-// 				repo.commit('File Updated', function (err) {
-// 					repo.commits(function (err, commits) {
-// 						return commits;
-// 					});
-// 				});
-// 			});
-// 		});
-// 	});
-// };
-//add comment
-// walk(__dirname, watchFilesAndCommit);
-//});
+		});
+	}
+}
