529025db0e951945d1e962a52841f53588e4ecca
Richard Moore <Rmoore424@gmail.com>
2015-03-19 15:38:32 -0400

    added console.logs to check

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ var path = require("path");
 var git = require('gift');
 var dir = require("node-dir");
 var repo = git(__dirname);
+console.log(repo);
 
 module.exports = {
 	filewatcher: function () {
 		console.log("is running");
 		if (repo == "undefined") {
+			console.log('repo is undefined');
 			throw new Error("No Git repository initialized");
 			return;
 		}
 		dir.files(__dirname, function (err, files) {
+			console.log("files", files);
+			console.log("Error", err);
 			if (err) throw err;
 			files.forEach(function (file) {
 				if (!file.match(/node_modules/) && !file.match(/.git/)) {
