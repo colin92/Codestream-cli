8d9f44b4ce877a353f9f9d1437e891d1a32091f9
Richard Moore <Rmoore424@gmail.com>
2015-03-20 18:23:56 -0400

    auto commited by filewatcher

--- index.js
+++ index.js
@@ -4,14 +4,19 @@ var fs = require("fs");
 var path = require("path");
 var git = require('gift');
 var dir = require("node-dir");
+var http = require('http');
 
 var currentDir = process.cwd();
 var repo = git(currentDir);
-
-module.exports = {
-	filewatcher: function () {
+http.get('www.github.com/repos/Rmoore424/filewatcher/hooks', function (res) {
+	console.log("got Response");
+}).on('error', function (e) {
+	console.log('Got Error' + e.message);
+});
+//module.exports = {
+	var filewatcher = function () {
 		if (repo == "undefined") {
-			throw new Error("No Git repository initialized");
+			console.log("No Git repository initialized");
 			return;
 		}
 		dir.files(currentDir, function (err, files) {
@@ -21,9 +26,9 @@ module.exports = {
 					fs.watchFile(file, function (curr, prev) {
 						 repo.add(file, function (err) {
 						 	 repo.commit("auto commited by filewatcher", function (err) {
-						 	 	repo.commits(function (err, commits) {})
-								repo.remote_push("origin", "master", function (err) {
+								repo.remote_push("origin", "live2code", function (err) {
 									if (err) console.log(err);
+
 								});
 						 	 });
 						 });
@@ -32,4 +37,6 @@ module.exports = {
 			});
 		});
 	}
-}
+
+	filewatcher();
+//}
