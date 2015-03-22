0fd84f686a28517bee104b6c54635e3dba861ff9
Richard Moore <Rmoore424@gmail.com>
2015-03-22 16:18:29 -0400

    auto committed by Codestream

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ var watch = require('watch');
 var currentDir = process.cwd();
 var repo = git(currentDir);
 
+var autoCommit = function (file) {
+	repo.add(file, function (err) {
+		if (err) console.log(err);
+		repo.commit("auto committed by Codestream", function (err) {
+				if (err) console.log(err);
+			repo.remote_push('origin', 'codestream', function (err) {
+				if (err) console.log(err);
+			});
+		});
+	});
+}
+
 watch.createMonitor(currentDir, {ignoreDotFiles: true, ignoreDirectoryPattern: /(node_modules)|(bower_components)/}, function (monitor) {
 	if (repo == "undefined") {
 		git.init(currentDir, function (err, repo) {
@@ -20,16 +32,12 @@ watch.createMonitor(currentDir, {ignoreDotFiles: true, ignoreDirectoryPattern: /
 	});
 
 	monitor.on('changed', function (file, curr, prev) {
-		repo.add(file, function (err) {
-			console.log(err);
-		});
+		autoCommit(file);
 	});
 
 });
 
-// var filewatcher = function () {
-
-// }
+//add comment
 //module.exports = {
 	// var filewatcher = function () {
 	// 	if (repo == "undefined") {
