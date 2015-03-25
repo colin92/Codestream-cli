06ba32382f221cf33b2aafa5771aa1701f935f71
Richard Moore <Rmoore424@gmail.com>
2015-03-25 11:19:51 -0400

    connected filewatcher to our server

--- index.js
+++ index.js
@@ -1,19 +1,125 @@
 #! /usr/bin/env node
 
 var git = require('gift');
+var GitHubApi = require('github');
+var Promise = require('bluebird');
 var watch = require('watch');
 var http = require('http');
+var request = require('request-promise');
 var prompt = require('prompt');
+var querystring = require('querystring');
+var express = require('express');
+var app = express();
+
+app.listen(3000);
+app.post('/login', function (req, res, next) {
+	console.log(req.body);
+	res.send([{'name': 'Repo1', "githubUrl": "123happytownlane"}, {'name': 'Repo2', 'githubUrl': '12323Alaska'}]);
+});
+
+app.post('repos/create', function (req, res, next) {
+	console.log(req.body);
+	res.send()
+})
 
 var currentDir = process.cwd();
 var repo = git(currentDir);
 
+var github = new GitHubApi({
+	version: "3.0.0"
+});
+
+//schemas for prompts
+var userSchema = {
+	properties: {
+		githubUsername: {
+			pattern: /^[a-zA-Z\s\-0-9]+$/,
+			description: "Enter your Github username".cyan,
+			required: true
+		},
+		githubPassword: {
+			hidden: true,
+			description: "Enter your Github password".cyan,
+			required: true
+		},
+		codestreamPassword: {
+			hidden: true,
+			description: "Enter your Codestream password".cyan,
+			required: true
+		}
+	}
+};
+
+var repoSchema = {
+	properties: {
+		repositoryName: {
+			description: "If your repository is included in the list above, type in the name of that repository, else type 'new'\n".green
+		}
+	}
+};
+
+var newRepoSchema = {
+	properties: {
+		newRepoName: {
+			description: "Enter the name of your new remote repository".cyan
+		}
+	}
+}
+
 prompt.start();
 
-prompt.get(['username', 'password'], function (err, result) {
-	console.log("Result ", result);
-	console.log('username ', + result.username);
-	console.log('password ' + result.password);
+prompt.get(userSchema, function (err, result) {
+
+	//basic authentication with github
+	github.authenticate({
+		type: "basic",
+		username: result.githubUsername,
+		password: result.githubPassword
+	});
+
+	var options = {
+		uri: 'http://localhost:3000/login',
+		body: JSON.stringify({
+			username: result.githubUsername,
+			password: result.codestreamPassword
+		}),
+		json: true
+	};
+
+	request.post(options)
+		.then(function (response) {
+			repositoryList = response;
+			console.log(repositoryList);
+			prompt.get(repoSchema, function (err, result) {
+				repositoryList.forEach(function (repository) {
+					if (repository.name === result.repositoryName) {
+
+						var newOptions = {
+							uri: 'http://localhost:3000/repos/start',
+							body: JSON.stringify({
+								repo: result.repositoryName
+							}),
+							json: true
+						}
+						request.post(newOptions)
+							.then(function (response) {
+								console.log(response);
+							}).catch(console.error);
+					}
+				});
+				if (result.repositoryName === 'new') {
+					prompt.get(newRepoSchema, function (err, result) {
+						github.repos.create({
+							name: result.newRepoName
+						}, function (err, data) {
+							repo.remote_add(data.name, data.ssh_url, function (err) {
+								if (err) console.log(err);
+							});
+						});
+					});
+				}
+			});
+		}).catch(console.error);
 });
 
 var autoCommit = function (file) {
@@ -27,7 +133,7 @@ var autoCommit = function (file) {
 		});
 	});
 }
-
+//Watches files for changes and pushes to Github
 // watch.createMonitor(currentDir, {ignoreDotFiles: true, ignoreDirectoryPattern: /(node_modules)|(bower_components)/}, function (monitor) {
 
 // 	monitor.on('created', function (file, stat) {
